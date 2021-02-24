80bf364348be6df5138c22f32c79e9243a1e0242
yzs <1064118784@qq.com>
2021-02-24 23:58:40 +0800

    服务初始化

---
+++ gulimall-ware/src/test/java/com/wxz/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxz.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
